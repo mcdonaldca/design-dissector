577ea91acc947ade5d0fdf7d3fa30620e878f0f7
Adele McDonald <caitlin.adele.mcdonald@gmail.com>
2020-04-24 17:04:11 -0400

    In this house we comment our code.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 function Dissector() {
   this.originalImage = $("#original");
   this.uploadInput = $("#upload");
-  this.uploadInput.on("change", this.imageUploaded.bind(this));
+  this.uploadInput.on("change", this.onFileUploaded.bind(this));
 }
 
 
-Dissector.prototype.imageUploaded = function() {
+/** Handler for when an image is uploaded. */
+Dissector.prototype.onFileUploaded = function() {
+  // Get the raw input element to access the files object.
   var uploader = this.uploadInput.get()[0];
+  // Check that the file is present.
   if (uploader.files && uploader.files[0]) {
     var uploadedImage = uploader.files[0];
     this.originalImage.attr("src", URL.createObjectURL(uploadedImage));
@@ -15,6 +18,7 @@ Dissector.prototype.imageUploaded = function() {
 };
 
 
+/** Handler for when the image source has loaded. */
 Dissector.prototype.onImageLoad = function() { 
   alert("image is loaded!");
 }
